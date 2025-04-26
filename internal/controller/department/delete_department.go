6f6ab25881bab3411e3f2ce98f5c1bda3f82789b
namf2001 <ndkhanh.dhtd15a6hn@sv.uneti.edu.vn>
2025-04-26 09:51:06 +0700

    Refactor department deletion logic: streamline error handling and update HTTP request examples in documentation

--- internal/controller/department/delete_department.go
+++ internal/controller/department/delete_department.go
@@ -2,30 +2,26 @@ package department
 
 import (
 	"context"
-	pkgerrors "github.com/pkg/errors"
 	"management-be/internal/repository/ent"
 )
 
 // DeleteDepartment deletes a department by its ID
 func (i impl) DeleteDepartment(ctx context.Context, id int) error {
+	// Check if the department exists
+	_, err := i.repo.Department().GetDepartmentByID(ctx, id)
+	if err != nil {
+		return err
+	}
+
 	// Execute the delete operation within a transaction
-	err := i.repo.WithTransaction(ctx, func(tx *ent.Tx) error {
-		departmentRepo := i.repo.Department()
-
-		// Check if department exists
-		_, err := departmentRepo.GetDepartmentByID(ctx, id)
+	err = i.repo.WithTransaction(ctx, func(tx *ent.Tx) error {
+		err = i.repo.Department().DeleteDepartment(ctx, id)
 		if err != nil {
-			return pkgerrors.WithStack(ErrDepartmentNotFound)
-		}
-
-		// Delete department
-		err = departmentRepo.DeleteDepartment(ctx, id)
-		if err != nil {
-			return pkgerrors.WithStack(ErrDepartmentNotDeleted)
+			return err
 		}
 
 		return nil
 	})
 
-	return err
+	return nil
 }
